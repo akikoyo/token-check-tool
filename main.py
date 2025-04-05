14b9e858edf9c360b778e5f0fafaa01f53ab1a60
akikoyo <prskma@outlook.com>
2025-04-06 00:28:47 +0900

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from gui import TokenManager
-from PyQt6.QtWidgets import QApplication
-
-if __name__ == '__main__':
-    app = QApplication([])
-    window = TokenManager()
-    window.show()
-    app.exec()
